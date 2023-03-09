45b524d149422dd5a75648e808535dbb4fc86e0f
paul-nhat-CLV <nhat.quach.tpv@one-line.com>
2023-03-09 17:56:26 +0700

    change to my own project, connection with postgres

--- models/analytics/products.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  a1.stock_item_id AS product_id,
-  a1.stock_item_name AS product_name,
-  a1.supplier_id,
-  a2.supplier_name,
-  COALESCE(brand,"Undefined") AS brand_name
-FROM `atomic-oven-374400.learn_dbt.warehouse_stock_items` AS a1
-LEFT JOIN {{ ref('supplier') }} AS a2
-  ON a1.supplier_id = a2.supplier_id
--- models/analytics/supplier.sql
+++
@@ -1,5 +0,0 @@
-SELECT
-  supplier_id
-  , UPPER(supplier_name) AS supplier_name
-  , supplier_category_id
-FROM `atomic-oven-374400.learn_dbt.purchasing_suppliers` 
