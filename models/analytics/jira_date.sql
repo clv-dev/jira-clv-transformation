c0ff4a89d2995cddc64b3f014d7e57c2a02e3355
looker.dev@a-ware.me <looker.dev@a-ware.me>
2023-05-31 03:29:29 +0000

    Change updatte Iteration logic

--- models/analytics/jira_date.sql
+++ models/analytics/jira_date.sql
@@ -49,19 +49,6 @@ SELECT
       ELSE 'Invalid' END
     AS is_holiday
   , IFNULL(dim_holiday.holiday_name, 'Undefined') AS holiday_name
-  , CASE
-      WHEN date BETWEEN '2023-02-15' AND '2023-02-28' THEN 'PI8 Iteration 1'
-      WHEN date BETWEEN '2023-03-01' AND '2023-03-14' THEN 'PI8 Iteration 2'
-      WHEN date BETWEEN '2023-03-15' AND '2023-03-28' THEN 'PI8 Iteration 3'
-      WHEN date BETWEEN '2023-03-29' AND '2023-04-11' THEN 'PI8 Iteration 4'
-      WHEN date BETWEEN '2023-04-12' AND '2023-05-07' THEN 'PI8 Iteration 5'
-      WHEN date BETWEEN '2023-05-08' AND '2023-05-14' THEN 'PI9 Planning Week'
-      WHEN date BETWEEN '2023-05-17' AND '2023-05-30' THEN 'PI9 Iteration 1'
-      WHEN date BETWEEN '2023-05-31' AND '2023-06-13' THEN 'PI9 Iteration 2'
-      WHEN date BETWEEN '2023-06-14' AND '2023-06-27' THEN 'PI9 Iteration 3'
-      WHEN date BETWEEN '2023-06-28' AND '2023-07-11' THEN 'PI9 Iteration 4'
-      WHEN date BETWEEN '2023-07-12' AND '2023-07-25' THEN 'PI9 Iteration 5'
-      ELSE 'Undefined' END
   AS dde_iteration
 FROM dim_date__extract_components AS dim_date
 LEFT JOIN `looker-team-management-386803.jira_clv_staging.jira_holiday` AS dim_holiday
