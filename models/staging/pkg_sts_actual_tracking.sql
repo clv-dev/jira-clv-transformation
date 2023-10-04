7be39370313ae78fb7e015f4a2273a1160b420cc
dgmkhanh <dgmkhanh@gmail.com>
2023-10-04 18:04:49 +0700

    Apply jinja and refactor script

--- models/staging/pkg_sts_actual_tracking.sql
+++ models/staging/pkg_sts_actual_tracking.sql
@@ -9,47 +9,20 @@ WITH pkg_sts_actual_raw AS (
   FROM pkg_sts_actual_raw 
   UNPIVOT (
     Actual_Status FOR Sprint IN (
-      PI8___Iteration_1,
-      PI8___Iteration_2,
-      PI8___Iteration_3,
-      PI8___Iteration_4,
-      PI8___Iteration_5,
 
-      PI9___Iteration_1,
-      PI9___Iteration_2,
-      PI9___Iteration_3,
-      PI9___Iteration_4,
-      PI9___Iteration_5,
+      {%- set pi_range = range(8,15) -%}
+      {%- set iter_range = range(1,6) -%}
 
-      PI10___Iteration_1,
-      PI10___Iteration_2,
-      PI10___Iteration_3,
-      PI10___Iteration_4,
-      PI10___Iteration_5,
-
-      PI11___Iteration_1,
-      PI11___Iteration_2,
-      PI11___Iteration_3,
-      PI11___Iteration_4,
-      PI11___Iteration_5,
-
-      PI12___Iteration_1,
-      PI12___Iteration_2,
-      PI12___Iteration_3,
-      PI12___Iteration_4,
-      PI12___Iteration_5,
-
-      PI13___Iteration_1,
-      PI13___Iteration_2,
-      PI13___Iteration_3,
-      PI13___Iteration_4,
-      PI13___Iteration_5,
-
-      PI14___Iteration_1,
-      PI14___Iteration_2,
-      PI14___Iteration_3,
-      PI14___Iteration_4,
-      PI14___Iteration_5
+      {% for pi in pi_range %}
+        {% for iter in iter_range %}
+          {% if (pi == pi_range[-1]) and (iter == iter_range[-1]) -%}
+      PI{{ pi }}___Iteration_{{ iter }}
+          {%- else -%}
+      PI{{ pi }}___Iteration_{{ iter }},
+          {%- endif -%}
+        {% endfor -%}
+      {% endfor %}
+      
     )
   )
 )
--- models/staging/pkg_sts_projection_tracking.sql
+++ models/staging/pkg_sts_projection_tracking.sql
@@ -9,47 +9,20 @@ WITH pkg_sts_projection_raw AS (
   FROM pkg_sts_projection_raw 
   UNPIVOT (
     Planning_Status FOR Sprint IN (
-      PI8_Iteration_1,
-      PI8_Iteration_2,
-      PI8_Iteration_3,
-      PI8_Iteration_4,
-      PI8_Iteration_5,
 
-      PI9_Iteration_1,
-      PI9_Iteration_2,
-      PI9_Iteration_3,
-      PI9_Iteration_4,
-      PI9_Iteration_5,
+      {%- set pi_range = range(8,15) -%}
+      {%- set iter_range = range(1,6) -%}
 
-      PI10_Iteration_1,
-      PI10_Iteration_2,
-      PI10_Iteration_3,
-      PI10_Iteration_4,
-      PI10_Iteration_5,
-
-      PI11_Iteration_1,
-      PI11_Iteration_2,
-      PI11_Iteration_3,
-      PI11_Iteration_4,
-      PI11_Iteration_5,
-
-      PI12_Iteration_1,
-      PI12_Iteration_2,
-      PI12_Iteration_3,
-      PI12_Iteration_4,
-      PI12_Iteration_5,
-
-      PI13_Iteration_1,
-      PI13_Iteration_2,
-      PI13_Iteration_3,
-      PI13_Iteration_4,
-      PI13_Iteration_5,
-
-      PI14_Iteration_1,
-      PI14_Iteration_2,
-      PI14_Iteration_3,
-      PI14_Iteration_4,
-      PI14_Iteration_5
+      {% for pi in pi_range %}
+        {% for iter in iter_range %}
+          {% if (pi == pi_range[-1]) and (iter == iter_range[-1]) -%}
+      PI{{ pi }}_Iteration_{{ iter }}
+          {%- else -%}
+      PI{{ pi }}_Iteration_{{ iter }},
+          {%- endif -%}
+        {% endfor -%}
+      {% endfor %}
+      
     )
   )
 )
